40514f422168d6c54bd0ac2857e108d5f0ee3d8e
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2018-09-25 23:53:23 -0500

    Add files via upload

--- hw2/q4/hw2_q4_b.c
+++ hw2/q4/hw2_q4_b.c
@@ -7,14 +7,15 @@ double MonteCarlo(int s)
 	double inCircle = 0;
 	double outCircle = 0;
 	int count = 0;
-	int R = 15000;
+	int R = RAND_MAX/2;
 
 #pragma omp parallel num_threads(10) shared(inCircle, outCircle, R, count)
 	{
 		int i;
 		for (i = omp_get_thread_num(); i < s; i += 10) {
-			int x = (rand() % (2*R)) - R;
-			int y = (rand() % (2*R)) - R;
+			/* rand() returns a num between 0 - RAND_MAX. We subtract RAND_MAX/2 to get an even split of pos and neg nums */
+			int x = rand() - R;
+			int y = rand() - R;
 
 			if (((x*x) + (y*y)) < (R*R)) {
 #pragma omp critical
