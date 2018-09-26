1c8d707f081f3b80f9a8015b231342de6b1ac1bd
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2018-09-25 23:39:06 -0500

    Add files via upload

--- hw2/q4/hw2_q4_b.c
+++ hw2/q4/hw2_q4_b.c
@@ -4,13 +4,48 @@
 
 double MonteCarlo(int s)
 {
-//Write your code here
+	double inCircle = 0;
+	double outCircle = 0;
+	int count = 0;
+	int R = 32767;
+
+#pragma omp parallel num_threads(10) shared(inCircle, outCircle, R, count)
+	{
+		int i;
+		for (i = omp_get_thread_num(); i < s; i += 10) {
+			int x = (rand() % (2*R)) - R;
+			int y = (rand() % (2*R)) - R;
+
+			if (((x*x) + (y*y)) < (R*R)) {
+#pragma omp critical
+				{
+					inCircle = inCircle + 1;
+					count++;
+
+				}
+			}
+			else {
+#pragma omp critical
+				{
+					outCircle = outCircle + 1;
+					count++;
+
+				}
+			}
+
+		}
+
+	}
+
+	return (inCircle / outCircle);
+
 }
 
 void main()
 {
 double pi;
-pi=MonteCarlo(100000000);
+//pi=MonteCarlo(100000000);
+pi = MonteCarlo(10000000000000);
 printf("Value of pi is: %lf\n",pi);
 }
 
