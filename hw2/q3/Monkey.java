0b535e3324b37dd4d2df00288ff34de5cb7d9e51
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2018-09-25 22:46:05 -0500

    Add files via upload

--- hw2/q3/Monkey.java
+++ hw2/q3/Monkey.java
@@ -1,16 +1,82 @@
 package q3;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Monkey {
+	
+	static int leftNum = 0;
+	static int rightNum = 0;
+	static boolean kongFlag = false;
+	static boolean kongWants = false;
+	ReentrantLock lock = new ReentrantLock();
+	Condition full = lock.newCondition();
+	Condition kong = lock.newCondition();
+	
 
     public Monkey() {
 
     }
 
     public void ClimbRope(int direction) throws InterruptedException {
+    	lock.lock();
+    	try {    	
+    		// Left Monkey
+    		if(direction == 0) {
+    			while(rightNum > 0 || (rightNum + leftNum == 3) || kongFlag) {full.await();}
+    			leftNum++;
+    		}
+    		// Right Monkey
+    		else if (direction == 1){
+    			while(leftNum > 0 || (rightNum + leftNum == 3) || kongFlag) {full.await();}
+    			rightNum++;
+    		}
+    		// Kong
+    		else {
+    			kongWants = true;
+    			while(leftNum + rightNum > 0) {kong.await();}
+    			kongFlag = true;
+    		}
+    			
+    		
+    	} finally {
+    		lock.unlock();
+    	}
+    	
 
     }
 
     public void LeaveRope() {
+    	try {
+    		lock.lock();
+    		if(leftNum > 0) {
+    			leftNum--;
+    			if(kongWants) {
+    				kong.signal();
+    			}
+    			else {
+    				full.signalAll();
+    			}
+    		}
+    		else if(rightNum > 0){
+    			rightNum--;
+    			if(kongWants) {
+    				kong.signal();
+    			}
+    			else {
+    				full.signalAll();
+    			}
+    		}
+    		else {
+    			kongWants = false;
+    			kongFlag = false;
+    			full.signalAll();
+    		}
+    					
+    		
+    	} finally {
+    		lock.unlock();
+    	}
 
     }
 
@@ -24,7 +90,12 @@ public class Monkey {
      * case 2: when Kong is on the rope, this value should be 1
      */
     public int getNumMonkeysOnRope() {
-        return -1;
+    	if(kongFlag) {
+    		return leftNum + rightNum + 1;
+    	}
+    	else {
+            return leftNum + rightNum; 		
+    	}
     }
 
 }
