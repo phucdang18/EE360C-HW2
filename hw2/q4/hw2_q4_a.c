f2e67f16e889f50478a06c7ac181eca2a0e207d4
phucdang18 <phucdang18@utexas.edu>
2018-09-27 05:11:10 -0500

    doesnt seem to increase with more threads

--- hw2/q4/hw2_q4_a.c
+++ hw2/q4/hw2_q4_a.c
@@ -1,12 +1,14 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys\timeb.h>
 
 double ** parseMatrix(int *row, int *col, FILE* fp) {
 	char c[2];
 	c[1] = '\0';
-	char* num = NULL; 
+	char* num = NULL;
 	int i, j;
 
 	while ((c[0] = fgetc(fp)) == ' ') {};
@@ -31,7 +33,7 @@ double ** parseMatrix(int *row, int *col, FILE* fp) {
 	for (i = 0; i < *row; i++) {
 		matrix[i] = (double *)calloc(1, *col * sizeof(double));
 	}
-	
+
 	i = 0;
 	j = 0;
 
@@ -39,7 +41,7 @@ double ** parseMatrix(int *row, int *col, FILE* fp) {
 		for (j = 0; j < *col;) {
 			c[0] = fgetc(fp);
 			if (c[0] != ' ' && c[0] != EOF && c[0] != '\n' && c[0] != NULL) {
-				num = (char*)calloc(1,100 * sizeof(char));
+				num = (char*)calloc(1, 100 * sizeof(char));
 				while (c[0] != ' ' && c[0] != EOF && c[0] != '\n') {
 					strcat(num, &c);
 					c[0] = fgetc(fp);
@@ -56,8 +58,8 @@ double ** parseMatrix(int *row, int *col, FILE* fp) {
 }
 
 
-void MatrixMult(char file1[],char file2[],int T)
-{	
+void MatrixMult(char file1[], char file2[], int T)
+{
 	FILE* fp;
 	int *row1, *col1, *row2, *col2;
 	int size = 0, i, j;
@@ -66,11 +68,11 @@ void MatrixMult(char file1[],char file2[],int T)
 	double ** result = NULL;
 	char *line = NULL;
 
-	row1 = calloc(1, sizeof(int));
-	col1 = calloc(1, sizeof(int));
-	row2 = calloc(1, sizeof(int));
-	col2 = calloc(1, sizeof(int));
-	
+	row1 = (int*) calloc(1, sizeof(int));
+	col1 = (int*) calloc(1, sizeof(int));
+	row2 = (int*) calloc(1, sizeof(int));
+	col2 = (int*) calloc(1, sizeof(int));
+
 	fp = fopen(file1, "r");
 	m1 = parseMatrix(row1, col1, fp);
 	fclose(fp);
@@ -85,9 +87,9 @@ void MatrixMult(char file1[],char file2[],int T)
 #pragma omp parallel num_threads(T)
 	{
 		int i, j, k;
-	    double sum;
+		double sum;
 		for (i = omp_get_thread_num(); i < *row1; i += T) {
-			for (j = 0; j < *col2; j ++) {
+			for (j = 0; j < *col2; j++) {
 				sum = 0;
 				for (k = 0; k < *row2; k++) {
 					sum += m1[i][k] * m2[k][j];
@@ -117,11 +119,20 @@ void MatrixMult(char file1[],char file2[],int T)
 
 void main(int argc, char *argv[])
 {
-  char *file1, *file2;
-  file1=argv[1];
-  file2=argv[2];
-  int T=atoi(argv[3]);
-  MatrixMult(file1,file2,T);
+	struct timeb start, end;
+	int diff;
+	char *file1, *file2;
+	file1 = argv[1];
+	file2 = argv[2];
+	int T = atoi(argv[3]);
+	ftime(&start);
+	MatrixMult(file1, file2, T);
+	ftime(&end);
+	diff = (int)(1000.0 * (end.time - start.time)
+		+ (end.millitm - start.millitm));
+
+	printf("\nOperation took %u milliseconds\n", diff);
+	printf("\nThread is %u\n", T);
 }
 
 
